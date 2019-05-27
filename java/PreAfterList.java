7efd016049373d6f9b34e15beff91901e88ca49b
青苗 <243194995@qq.com>
2019-05-27 09:27:17 +0800

    更新 PreAfterList.java

--- java/PreAfterList.java
+++ java/PreAfterList.java
@@ -4,11 +4,11 @@ import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 
 public class PreAfterList {
 
-    // 需求：根据 ID 获取指定 ID 文章的包括当前 ID 及上一条和下一条内容集合
+    // 需求：根据 ID 获取指定 ID 文章，包括当前 ID 及上一条和下一条的内容集合
     // 原样贴出天才同事的操作如下，看完肺都气炸了！！ 看官们你觉得正确做法是什么？？
     public ArticleVO getArticleVO(Long id) {
         ArticleVO vo = new ArticleVO(getById(id));
-        List<Article> list = articleMapper.selectList(baseMapper.selectList(Wrappers.<Article>lambdaQuery().orderByDesc(Article::getCreateTime)));
+        List<Article> list = articleMapper.selectList(Wrappers.<Article>lambdaQuery().orderByDesc(Article::getCreateTime));
         if (list.size() == 1) {
             return vo;
         } else if (list.size() == 2) {
