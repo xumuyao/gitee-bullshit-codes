3b222e744a9ad69142bd5759e1f1f6292ed8e1a9
sunjian126 <sunjian11@jd.com>
2019-05-27 14:23:44 +0800

    add golang demo

--- golang/fuck_recover.go
+++ golang/fuck_recover.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+var m = 0
+
+func main() {
+	//defer 方法中的参数方法会被执行时候县渲染。 这个时候返回的是0 而不是1
+	defer doSome(getValue())
+	m = 1
+	fmt.Println("end")
+}
+
+func doSome(value int) {
+	fmt.Println(value)
+}
+
+func getValue() int {
+	return m 
+}
--- golang/waht_err.go
+++ golang/waht_err.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+//参考 https://books.studygolang.com/advanced-go-programming-book/appendix/appendix-a-trap.html
+func main() {
+	fmt.Println(errMehthod())
+}
+
+func errMehthod() (err error) {
+	if err := Bar(); err != nil {
+		//在局部作用域中，命名的返回值内同名的局部变量屏蔽：
+		//这里运行时会报错。编译不报错
+		return
+	}
+	return
+}
+
+func Bar() error {
+	return fmt.Errorf("fuck hongshu")
+}
