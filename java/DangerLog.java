1417d9d9fa363ebe4b796b26a061823060bd5d6e
lilei <lilei.zh@qq.com>
2019-05-28 16:30:44 +0800

    删除不需要的日志代码或者调试时添加监视都会导致业务出错

---
+++ java/DangerLog.java
@@ -0,0 +1,19 @@
+public class DangerLog {
+	public class ReadLog {
+		int readNum;
+
+		@Override
+		public String toString() {
+			// 调用该方法将对象的状态，打印或者调试都可能会调用该方法
+			readNum++;
+			return "ReadLog [readNum:" + readNum + ",其他信息]";
+		}
+	}
+	
+	public ReadLog getReadLog() {
+		ReadLog log = new ReadLog();
+		// 删除下面不需要的日志代码或者调试时添加监视都会导致业务出错
+		System.out.println(log);
+		return log;
+	}
+}
