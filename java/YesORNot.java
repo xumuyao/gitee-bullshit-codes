7136a152fdc7a1c2c8db559f4fe0f91db04e7d93
fan_sten <15626131368@163.com>
2019-05-29 09:39:12 +0800

    添加判断结果为真时，新手经常会写的代码

---
+++ java/YesORNot.java
@@ -0,0 +1,12 @@
+/**
+ * 导师：写一个判断对象是否为空的方法</br>
+ * 学员：好的，这就开始写
+ * ....
+ * 导师：额~，你再看看书吧
+ */
+class YesORNot{
+
+    public boolean isNull(Object object) {
+        return object == null ? true : false;
+    }
+}
