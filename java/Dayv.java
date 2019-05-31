850c1d0ac707eea2f69ed9b226d23610bd41d446
dawu <958667514@qq.com>
2019-05-31 10:11:28 +0800

    19.5.31

---
+++ java/Dayv.java
@@ -0,0 +1,10 @@
+package com.test.java.enumtest;
+
+
+public class Dayv {
+	
+	//记得刚工作的时候踩过这样一个坑 当时debug发现相等数值情况下有时返回true 有时返回false 
+	public boolean isEqual(Integer a,Integer b){
+		return a==b;
+	}
+}
