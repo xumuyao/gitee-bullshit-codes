584109b4388b4116f3fdecb5b376010758b38811
fage <943626451@qq.com>
2019-05-31 13:52:42 +0800

    去你妹的外包吧

---
+++ php/FastProductController.php
@@ -0,0 +1,35 @@
+<?php
+namespace Owner\Controller;
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2019/5/30
+ * Time: 14:38
+ */
+class FastProductController extends BaseController {
+
+
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+
+    /**
+     * 产品列表
+     */
+    public function prodctList(){
+        $sleep = I('get.optimizeVersion','1');//迭代版本号,迭代版本越高,速度越快,方便快速优化,最多给客户优化十次
+        if($sleep>0){
+            sleep(10-$sleep);
+        }
+        $productId = I('get.productId');
+        $userInfo = db('product')->where(['id'=>$productId])->select();
+        echo json_encode(
+            [
+                'state'=>1100,
+                'message'=>'查询成功',
+                'data'=>$userInfo
+            ]
+        );
+    }
