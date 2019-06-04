a789d698e996e741b59a5c8461d38aed4f985a69
郭鹏 <gp@nidong.com>
2019-06-04 11:19:01 +0800

    equals判断integer和String，输出跟debug值都是一样的，就是不进

---
+++ java/Equals.java
@@ -0,0 +1,14 @@
+package com.num.controller;
+
+public class Equals {
+
+	//equals判断integer和String，输出跟debug值都是一样的，就是不进
+    public static void main(String[] args) {
+        String a = "1";
+        int b = 1;
+        if(a.equals(b)){
+            //死活不进
+        } else {
+        }
+    }
+}
