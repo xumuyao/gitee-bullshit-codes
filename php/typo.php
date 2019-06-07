9d21fc37d5873118063638b571172d1a3e4bb21c
ufo <qufo@163.com>
2019-06-07 12:33:06 +0800

    TYPO

---
+++ php/typo.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * CET4 是找侯震画的。
+ */
+
+class Order {
+
+    /* 用户地址 */
+    public $adress;
+
+    public $is_pay;
+
+    // ......
+
+    public $ispay;
+
+    // .....
+
+    public $is_paid;
+
+    // ......
+
+    public $ispaid;
+
+    // ......
+
+    public $pay_time;
+
+    // ......
+
+    public $pay_timesatmp;
+
+}
+
+
+class Goods {
+
+    public $pirce;
+
+}
