d7b0b36525eb06977034ffff65596a2d3171e51b
chenjiehan <chenjiehan007@gmail.com>
2019-06-12 15:17:18 +0800

    某人（离职）写业务，不留自己的姓名、手机号，留你的信息的代码

---
+++ java/ContactAdmin.java
@@ -0,0 +1,17 @@
+/**
+ * 某人（离职）写业务，不留自己的姓名、手机号，留你的信息的时候
+ */
+public class ContactAdmin  {
+
+    /**
+     * @author 你的名字（实际是别人写的）
+     * @param args
+     */
+    public static void main(String[] args) {
+        try{
+            //业务
+        }catch (Exception e){
+            throw new Business("出错了，请联系管理员XXX，电话189xxxxxxxx")
+        }
+    }
+}
