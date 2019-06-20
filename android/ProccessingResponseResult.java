9be3397c000c9de820d1ae68d134a9a7d4d42444
MichaelZhang <zhangfuchao_flexi@163.com>
2019-06-20 10:50:15 +0800

    接手外包的项目，没有addAll()怎么破？

---
+++ android/ProccessingResponseResult.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class ProccessingResponseResult {
+
+    private List<String> dataSouce = new ArrayList<>();
+
+    private void processingResponseResult(List<String> result){
+
+        for (int i = 0; i < result.size(); i++) {
+            dataSouce.add(result.get(i));
+        }
+
+        /*处理网络请求后解析的数据，存在于N个文件，addAll()很难用，😂*/
+
+    }
+}
