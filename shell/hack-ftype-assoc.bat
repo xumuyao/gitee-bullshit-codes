185be603930855610b2d8d2cd5580d532df2502c
阿蒙 <ahmeng@aliyun.com>
2019-06-21 16:57:06 +0800

    ftype txtfile=D:\hackpad.exe %1 && assoc .html=txtfile 利用ftype和assoc两个CMD命令制作坑爹的入门级黑客程序

---
+++ shell/hack-ftype-assoc.bat
@@ -0,0 +1,12 @@
+C:\Windows\system32>ftype txtfile
+txtfile=%SystemRoot%\system32\NOTEPAD.EXE %1
+
+C:\Windows\system32>ftype txtfile=D:\hackpad.exe %1
+txtfile=D:\hackpad.exe %1
+
+C:\Windows\system32>assoc .html
+.html=htmlfile
+
+C:\Windows\system32>assoc .html=txtfile
+.html=txtfile
+
