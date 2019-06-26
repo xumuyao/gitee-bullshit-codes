f0b0b6cf762172bcb50ad2d1e235dc804de41110
Admin <494343093@qq.com>
2019-06-26 08:40:31 +0800

    叫某位学生创建一个拥有三个元素的数组 叫某位学生创建一个拥有三个元素的数组，弄得我哭笑不得

---
+++ java/createArray.java
@@ -0,0 +1,14 @@
+
+public class createArray {
+
+		/**
+		 * 
+		 * 创建一个拥有三个元素的数组
+		 * 
+		 * @param args
+		 */
+		public static void main(String[] args) {
+			String[] arr={"a,b,c"};
+			System.out.println(arr[1]);
+		}
+}
