07ec25f2799b06c682ce67cc35b3633f68773333
liuyang <popoliuyang@qq.com>
2019-09-24 16:51:44 +0800

    Long转String

---
+++ java/ChangeString.java
@@ -0,0 +1,13 @@
+/**
+ * Create Time 2019/9/24
+ *  Long转String
+ * @author liuyang
+ **/
+public class ChangeString {
+    public static void main(String[] ares){
+
+        Long a=1L;
+        String b=null;
+        b=a+"";
+    }
+}
