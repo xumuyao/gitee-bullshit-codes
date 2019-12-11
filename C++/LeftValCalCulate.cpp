b23f82feb9cc12b65d02a2e512f81491669189bb
qiangwanggo <932251594@qq.com>
2019-12-11 23:47:22 +0800

    update_value

--- C++/LeftValCalCulate.cpp
+++ C++/LeftValCalCulate.cpp
@@ -1,9 +1,9 @@
-bool TestFunc()
+﻿bool TestFunc()
 {
     //条件运算时，将作为左值变量放在右边避免出错
-    int nVal = 2;
+    int nVal = 1;
     //编译通过，但会始终返回false
-    if(3 = nVal)
+    if(2 = nVal)
         return false;
     return true;
 }
