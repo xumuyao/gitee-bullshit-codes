af48f7cac860bdc37aa7ef77d6dd125f0dda98ec
wx <2450416439@qq.com>
2019-12-12 10:52:44 +0800

    update_value2

--- C++/LeftValCalCulate.cpp
+++ C++/LeftValCalCulate.cpp
@@ -1,9 +1,9 @@
 bool TestFunc()
 {
     //条件运算时，将作为左值变量放在右边避免出错
-    int nVal = 0;
+    int nVal = 11;
     //编译通过，但会始终返回false
-    if(1 = nVal)
+    if(12 = nVal)
         return false;
     return true;
 }
