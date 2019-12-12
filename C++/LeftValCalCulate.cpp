2974407886b14fd527d036fd1a27dbb0b9b10351
wx <2450416439@qq.com>
2019-12-12 16:39:43 +0800

    评论任务 comment #I16LX0

--- C++/LeftValCalCulate.cpp
+++ C++/LeftValCalCulate.cpp
@@ -1,9 +1,9 @@
 bool TestFunc()
 {
     //条件运算时，将作为左值变量放在右边避免出错
-    int nVal = 11;
+    int nVal = 10;
     //编译通过，但会始终返回false
-    if(22 = nVal)
+    if(20 = nVal)
         return false;
     return true;
 }
