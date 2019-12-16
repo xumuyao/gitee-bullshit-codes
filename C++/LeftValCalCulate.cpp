99d3061227836db8bcf637b66b02fdd510e9655e
wx <2450416439@qq.com>
2019-12-16 15:40:37 +0800

    commit a_branch

--- C++/LeftValCalCulate.cpp
+++ C++/LeftValCalCulate.cpp
@@ -1,9 +1,9 @@
 ﻿bool TestFunc()
 {
     //条件运算时，将作为左值变量放在右边避免出错
-    int nVal = 15;
+    int nVal = 18;
     //编译通过，但会始终返回false
-    if(16 = nVal)
+    if(19 = nVal)
         return false;
     return true;
 }
